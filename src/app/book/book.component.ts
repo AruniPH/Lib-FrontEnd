e2b076afa89c9a1c3eb7f537ed7f63131490ff8c
AruniPH <aruniprasangikanmd@gmail.com>
2024-12-27 00:06:29 +0530

    Book Create Completed.

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BookRepresentation } from '../module/book.representation';
+
+import { FormBuilder } from '@angular/forms';
+import swal from 'sweetalert';
+import { BookService } from '../service/book.service';
+
 
 @Component({
   selector: 'app-Book',
@@ -7,5 +13,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './book.component.css'
 })
 export class BookComponent implements OnInit {
+  searchTerm: any;
+  BookObj: BookRepresentation = {};
+  Books: Array<any> = [];
+  allStatus: any;
 
+  statusValue: any;
+  isEditBook: boolean = false;
+  dtDynamicVerticalScrollExample: any;
+
+  constructor(
+    private bookService: BookService,
+    public fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.isEditBook == false;
+  
+  }
+
+  SaveBook(): void {
+    if (this.isEditBook) {
+    
+    } else {
+      swal({
+        title: "Are you sure?",
+        text: "Do you want to add this Book?",
+        icon: "warning",
+        dangerMode: true,
+      }).then((willAdd: any) => {
+        if (willAdd) {
+          this.bookService.createBook(this.BookObj).subscribe(() => {
+           
+            swal("Success!", "Book has been added!", "success");
+            this.ClearForm();
+          });
+        }
+      });
+    }
+  }
+
+
+
+  ClearForm(): void {
+    this.BookObj = {};
+    this.isEditBook = false;
+  }
 }
